57e10b431c4541714f7daff99435c1f6011f9d3b
nav <navid.shokouhi@utdallas.edu>
2016-04-12 21:58:47 -0500

    removed old run.sh file.

--- code/kaldi_setup/run.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/bash
-
-cat data/train/wav.scp | perl -ne 'if(m/(\S+)\s(\S+)/){print ". ~/.bashrc; python /scratch2/nxs113020/pyknograms/code/kaldi_setup/local/pykno_feat_extraction.py $_"}' > train_pykno_jobs.txt
